449a05ebf39646079f88ed269bd2c20ef8d983f4
Cody Schindler <admin@codyschindler.com>
2015-12-03 23:03:04 -0800

    deleted test section

--- app/board.py
+++ app/board.py
@@ -3,8 +3,8 @@ import random
 
 class Board(object):
     """a mine sweeper board"""
-    EMPTY_SPACE = "."
-    MINE_SPACE = "*"
+    EMPTY_SPACE = "0"
+    MINE_SPACE = "."
 
     def __init__(self, rows, columns):
         self.board = ""
@@ -12,16 +12,16 @@ class Board(object):
         self.rows = rows
         self.columns = columns
         self.possible_mines = random.randint(1, (rows * 2))
+        self.generate_new_board()
+        self.calc_hints()
+        self.print_board()
 
-
-# recommend replacing logic here with more straightwforward  sweeper
     def generate_new_board(self):
+        '''generates the board with mines and placeholders for numbers'''
         new_board_arr = []
         possible_mines = self.possible_mines
-
         for i in range(self.rows):
             row = []
-
             for j in range(self.columns):
                 if round(random.random(), 2) >= 0.4:
                     row.append(self.EMPTY_SPACE)
@@ -31,25 +31,30 @@ class Board(object):
                         possible_mines -= 1
                     else:
                         row.append(self.EMPTY_SPACE)
-
-            row_str = "".join(row)
-            new_board_arr.append(row_str)
-
-        # new_board_str = new
-        # self.actual_mines = sum()
-
+            new_board_arr.append(row)
         self.board = new_board_arr
-        print(self.board)
 
     def print_board(self):
-        board_arr = self.board
-        board_string = "\n".join(self.board)
-
-        print(board_string)
-
-
-# TEST STUFF
-test_board = Board(4,4)
-test_board.generate_new_board()
-test_board.print_board()
-
+        '''pretty-prints the board'''
+        new_arr = []
+        for row in self.board:
+            new_arr.append(' '.join(row))
+        for row_str in new_arr:
+            print(row_str)
+        
+    def calc_hints(self):
+        '''generate numbers after a board is generated'''
+        for i in range(self.rows):
+            for j in range(self.columns):
+                #when a mine is selected do the following
+                if(self.board[i][j] == self.MINE_SPACE):
+                    # k and l loops to access surrounding array indices
+                    for k in range(-1, 2):
+                        for l in range(-1, 2):
+                            #test for edges, continue if inaccessible
+                            if(i+k < 0 or j+l < 0 or i+k > self.rows-1 or j+l > self.columns-1):
+                                continue
+                            if (self.board[i+k][j+l] != self.MINE_SPACE):
+                                self.board[i+k][j+l] = str(int(self.board[i+k][j+l]) + 1)
+                else:
+                    continue
