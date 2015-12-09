3f547932472e75833a55745cfbab05992f21d286
Cody Schindler <admin@codyschindler.com>
2015-12-08 20:58:01 -0800

    added tests, refactored generator a bit

--- app/board.py
+++ app/board.py
@@ -10,36 +10,42 @@ class Board(object):
     DEFAULT_COLS = 9
     DEFAULT_MINES = 10
 
-    def __init__(self, rows = DEFAULT_ROWS, columns = DEFAULT_COLS, mines = DEFAULT_MINES, fclick = [4,4]):
+    def __init__(self, rows = DEFAULT_ROWS, columns = DEFAULT_COLS, mines = DEFAULT_MINES, fclick = [0,0], test_board = None):
+        self.test_board = test_board
         self.board = []
         self.mines = mines
         self.rows = rows
         self.columns = columns
         self.first_click = fclick
-        self.generate_new_board()
+        self.generate_new_random_board()
         self.calc_hints()
-        self.print_board()
+        self.print_board("0","*")
 
-    def generate_new_board(self):
-        '''generates the board with mines and placeholders for numbers'''
-        # create local variables for better readability
-        rows = self.rows
-        cols = self.columns
-        mines = self.mines
-        fclick = (self.first_click[0] * (cols)) + self.first_click[1]
-        #seed list with appropriate numbers of mines and spaces
-        for i in range(rows*cols-mines):
+    def populate_mines(self):
+        for i in range(self.rows*self.columns-self.mines):
             self.board.append(self.EMPTY_SPACE)
-        for j in range(mines):
+        for j in range(self.mines):
             self.board.append(self.MINE_SPACE)
         random.shuffle(self.board)
+
+    # def generate_new_static_board(self):
+
+    def generate_new_random_board(self):
+        '''generates the board with mines and placeholders for numbers'''
+        fclick = (self.first_click[0] * (self.columns)) + self.first_click[1]
+        # seed list with appropriate numbers of mines and spaces
+        if self.test_board == None:
+            self.populate_mines()
+        else:
+            self.board = self.test_board
+
         # clear the first-clicked space if necessary
         if(self.board[fclick] == self.MINE_SPACE):
             first_zero = self.board.index(self.EMPTY_SPACE)
             self.board[first_zero] = self.MINE_SPACE
             self.board[fclick] = self.EMPTY_SPACE
         #reshape the board into rows and columns
-        self.board = reshape(self.board, (rows, cols))
+        self.board = reshape(self.board, (self.rows, self.columns))
 
     def print_board(self, zeroes = EMPTY_SPACE, mines = MINE_SPACE):
         '''pretty-prints the board'''
@@ -55,13 +61,16 @@ class Board(object):
             for j in range(self.columns):
                 #when a mine is selected do the following
                 if(self.board[i][j] == self.MINE_SPACE):
-                    # k and l loops to access surrounding array indices
-                    for k in range(-1, 2):
-                        for l in range(-1, 2):
-                            #test for edges, continue if inaccessible
-                            if(i+k < 0 or j+l < 0 or i+k > self.rows-1 or j+l > self.columns-1):
-                                continue
-                            if (self.board[i+k][j+l] != self.MINE_SPACE):
-                                self.board[i+k][j+l] = str(int(self.board[i+k][j+l]) + 1)
+                    self.add_one_to_surrounding(i, j)
                 else:
                     continue
+
+    def add_one_to_surrounding(self, row, col):
+        # k and l loops to access surrounding array indices
+        for k in range(-1, 2):
+            for l in range(-1, 2):
+                #test for edges, continue if inaccessible
+                if(row+k < 0 or col+l < 0 or row+k > self.rows-1 or col+l > self.columns-1):
+                    continue
+                if(self.board[row+k][col+l] != self.MINE_SPACE):
+                    self.board[row+k][col+l] = str(int(self.board[row+k][col+l]) + 1)
--- test/test_board.py
+++ test/test_board.py
@@ -1,9 +1,18 @@
 # internal dependencies
-from app/board
+from app import board
 
 # external dependencies
 import unittest
 
 class TestBoard(unittest.TestCase):
-    def setUp(self):
-        self.board = Board
+
+	def test_board(self):
+		test = board.Board(15,30,99)
+		print("")
+		print(test.board)
+		self.assertEqual("abc","def")
+    # def set_up(self):
+    #     self.board = Board
+
+if __name__ == '__main__':
+	unittest.main()
